ef31b7a1aaae7fc1d22924dc67a5fb1663f992c0
samuel okolie <uche7877@gmail.com>
2022-06-24 14:52:19 +0100

    made modifications

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,7 @@
 let fullName = "Okolie Samuel Uchenna";
 let height = "5feet 8inches";
 let country = "Nigeria";
-Bio = () => {
+bio = () => {
   document.getElementById("myName").innerHTML = fullName;
   document.getElementById("myHeight").innerHTML = height;
   document.getElementById("myCountry").innerHTML = country;
